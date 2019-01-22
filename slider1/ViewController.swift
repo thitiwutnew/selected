aac97f2e49c41296df6fbaf3cdc4beed884e7b43
Admin <admin@MAC17.local>
2019-01-22 09:47:53 +0700

    Initial Commit

---
+++ slider1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slider1
+//
+//  Created by Admin on 22/1/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ slider1Tests/slider1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  slider1Tests.swift
+//  slider1Tests
+//
+//  Created by Admin on 22/1/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import slider1
+
+class slider1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

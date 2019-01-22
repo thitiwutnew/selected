b173eab390819e7d3f1e9e9bc237266c638c0851
Admin <admin@MAC17.local>
2019-01-22 10:02:37 +0700

    first commit

--- slider1/ViewController.swift
+++ slider1/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var BL: UILabel!
+    @IBAction func slider(_ sender: UISlider) {
+        BL.text = String(Int(sender.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
